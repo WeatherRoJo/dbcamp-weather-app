c2afdbd3d68b60b3e011fcdf2bb81a8203640602
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-04-13 02:53:23 -0300

    💄 feat: css footer and header

--- src/ui/components/surfaces/Footer/index.tsx
+++ src/ui/components/surfaces/Footer/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import DBlogo from '../../../assets/DBLogo.png';
+import "./style.css"
 
 export function Footer () {
     return(
-        <footer>
+        <footer className='footer'>
             <p> MADE WITH LOVE </p> 
             <img src={DBlogo} alt='Logo DB' />
         </footer>
--- src/ui/components/surfaces/Header/index.tsx
+++ src/ui/components/surfaces/Header/index.tsx
@@ -1,18 +1,16 @@
 import { Link } from "react-router-dom";
-
+import "./style.css"
 
 
 function Header(){
     return(
-        <div>
-            <header>
+            <header className="header" >
                 <ul>
                     <li> <Link to = "/" > Home </Link> </li>
                     <li> <Link to = "/register" > Cadastrar  </Link> </li>
                     <li> <Link to = "/list" >  Listar  </Link></li>
                 </ul>
             </header>
-        </div>
     )
 }
 
